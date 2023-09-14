5785163633d1f767c56e211750832ccb15d80618
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-09-14 18:09:00 +0200

    Style: rename function to run_a_star_for_task

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.Map import Map_Obj
 from src.a_star import a_star
 
-def run_astar_for_task(task_num):
+def run_a_star_for_task(task_num):
     # Instantiate a map object for task 1
     task_map = Map_Obj(task_num)
     # Run the A* algorithm and find the path
@@ -11,8 +11,8 @@ def run_astar_for_task(task_num):
     task_map.show_map()
 
 if __name__ == '__main__':
-    run_astar_for_task(1)
-    run_astar_for_task(2)
-    run_astar_for_task(3)
-    run_astar_for_task(4)
-    run_astar_for_task(5)
+    run_a_star_for_task(1)
+    run_a_star_for_task(2)
+    run_a_star_for_task(3)
+    run_a_star_for_task(4)
+    run_a_star_for_task(5)
