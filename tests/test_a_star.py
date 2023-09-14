f6fade77ebae106424eb8a4b8e50c051335f578c
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-09-14 12:39:09 +0200

    Feat: Add tests for a star heuristic

--- tests/test_a_star.py
+++ tests/test_a_star.py
@@ -1,24 +1,41 @@
 import pytest
-from src.Map import Map_Obj, Frontier
+from src.a_star import a_star_heuristic, reconstruct_path, Frontier
 
-def test_frontier_init():
+# def test_frontier_init():
+#     # Arrange
+#     map = Map_Obj(task=1)
+#     start_pos = map.get_start_pos()
+#     frontier = Frontier(start_pos, map)
+#     # Act
+#     empty_frontier = frontier.get_frontier()
+#     # Assert
+#     assert empty_frontier == [start_pos]
+
+
+
+def test_a_star_heuristic_no_difference():
     # Arrange
-    map = Map_Obj(task=1)
-    start_pos = map.get_start_pos()
-    frontier = Frontier(start_pos, map)
+    start_pos = [0, 0]
     # Act
-    empty_frontier = frontier.get_frontier()
-    # Assert
-    assert empty_frontier == [start_pos]
-
-
-
-def test_a_star_heuristic():
-    # Arrange
-    map = Map_Obj(task=1)
-    start_pos = map.get_start_pos()
-    frontier = Frontier(start_pos, map)
-    # Act
-    manhattan_distance = frontier.manhattan_distance(start_pos, start_pos)
+    manhattan_distance = a_star_heuristic(start_pos, start_pos)
     # Assert
     assert manhattan_distance == 0
+
+
+def test_a_star_heuristic_difference():
+     # Arrange
+    start_pos = [0, 0]
+    goal_pos = [5, 5]
+    # Act
+    manhattan_distance = a_star_heuristic(start_pos, goal_pos)
+    # Assert
+    assert manhattan_distance == 10
+
+def test_a_star_heuristic_negative_difference():
+    # Arrange
+    start_pos = [0, 0]
+    goal_pos = [-5, 5]
+    # Act
+    manhattan_distance = a_star_heuristic(start_pos, goal_pos)
+    # Assert
+    assert manhattan_distance == 10
